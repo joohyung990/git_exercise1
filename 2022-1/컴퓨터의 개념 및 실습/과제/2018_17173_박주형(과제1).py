a28e074a15be16857b6cf6c66e630d87e602be8d
joohyung990 <pjh990@snu.ac.kr>
2022-05-31 17:07:48 +0900

    3승

--- 2022-1/컴퓨터의 개념 및 실습/과제/2018_17173_박주형(과제1).py
+++ 2022-1/컴퓨터의 개념 및 실습/과제/2018_17173_박주형(과제1).py
@@ -1,5 +1,7 @@
 import math
 a=int(input("가로의 길이를 입력하세요:"))
 b=int(input("세로의 길이를 입력하세요:"))
-c=math.sqrt((a**2)+(b**2))
+c=math.sqrt((a**3)+(b**3))
 print("빗변의 길이는 ", c, "입니다.", sep="")
+
+print("끝")
